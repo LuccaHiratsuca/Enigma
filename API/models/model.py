ff2bf22da66f1473dbcd2d04bb11e9d607555c28
FeMCDias <femcdias@gmail.com>
2023-03-12 14:51:10 -0300

    feat: models.py update functions from main

---
+++ API/models/model.py
@@ -0,0 +1,28 @@
+from functools import reduce
+import numpy as np
+
+def para_one_hot(msg: str) -> np.array:
+    return np.array([[1 if {letra: indice for indice, letra in enumerate('abcdefghijklmnopqrstuvwxyz ')}[j] == i else 0 for i in range(27)] for j in msg.lower()]).T
+
+def para_string(M: np.array) -> str:
+    return ''.join({indice: letra for indice, letra in enumerate('abcdefghijklmnopqrstuvwxyz ')}[i] for i in np.argmax(M, axis=0))
+
+def cifra(msg: str, M: np.array) -> str:
+    return para_string(M @ para_one_hot(msg))
+
+def de_cifra(msg: str, M: np.array) -> str:
+    return para_string(np.linalg.inv(M) @ para_one_hot(msg))
+
+def enigma(msg: str, P: np.array, E: np.array) -> str:
+    hotMessage = para_one_hot(msg).T
+    final = P @ hotMessage[0]
+    for i in range(1,hotMessage.shape[0]):
+        final = np.vstack((final, reduce(lambda x, : E @ x, range(i), P @ hotMessage[i]).T))
+    return para_string(final.T)
+
+def de_enigma(msg: str, P: np.array, E: np.array) -> str:
+    msg = para_one_hot(msg).T
+    final = np.linalg.inv(P) @ msg[0]
+    for i in range(1,msg.shape[0]):
+        final = np.vstack((final, np.linalg.inv(P) @ reduce(lambda x, : np.linalg.inv(E) @ x, range(i), msg[i])))
+    return para_string(final.T)
