122efab0562e031bbc3e5d576daf5d6240d66c31
FeMCDias <femcdias@gmail.com>
2023-03-12 11:37:58 -0300

    feat: de_cifra

--- main.py
+++ main.py
@@ -22,5 +22,7 @@ def para_one_hot(msg: str) -> np.array:
    return np.array([[1 if msg[i] == ('abcdefghijklmnopqrstuvwxyz')[j] else 0 for j in range(26)] for i in range(len(msg))]).T
 
 def para_string(M: np.array) -> str:
-    return ''.join([('abcdefghijklmnopqrstuvwxyz')[np.where(M[:,i] == 1)[0][0]] for i in range(M.shape[1])])
+   return ''.join([('abcdefghijklmnopqrstuvwxyz')[np.where(M[:,i] == 1)[0][0]] for i in range(M.shape[1])])
 
+def de_cifra(msg: str, M: np.array) -> str:
+   return para_string(np.linalg.inv(M) @ para_one_hot(msg))
