ca3f52426efea8612f1e7cf9a4510e97bc539a6d
FeMCDias <femcdias@gmail.com>
2023-03-12 17:02:30 -0300

    feat: biblioteca doc

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="enigma_hiratsuca_maluli",
+    version="0.0.1",
+    authors="Hiratsuca, Maluli",
+    description="A package to encrypt and decrypt messages using the Enigma Machine and Linear Algebra",
+    url="https://github.com/LuccaHiratsuca/Enigma",
+    packages=['enigma_hiratsuca_maluli'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=['numpy']
+)
